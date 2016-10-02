1fcc4c0e54ea067099c9b3a014451dc71971bd70
jonathan.deng <dengjonathan@gmail.com>
2016-10-02 11:12:02 -0400

    first commit

---
+++ app/state.js
@@ -0,0 +1,37 @@
+const {createStore} = Redux;
+
+const counter = (state=0, action) => {
+  switch(action.type) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counter);
+
+const Counter = ({
+  value,
+  onIncrement,
+  onDecrement
+}) => {
+  return (
+  <div>
+    <h1> {value} </h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
+)};
+
+const render = () => {ReactDom.render( <Counter
+  value={store.getState()}
+  onIncrement={()=> store.dispatch({type: 'increment'})}
+  onDecrement={() => store.dispatch({type: 'decement'})}
+  />, document.getElementById('app'));
+};
+
+store.subscribe(render);
+render();
